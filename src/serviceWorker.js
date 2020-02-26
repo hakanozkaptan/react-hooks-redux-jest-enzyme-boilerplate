ed4f01bb9b461030474e7938b47e247bf0603242
Hakan Ozkaptan <ozkaptanhakan@gmail.com>
2020-02-26 10:13:29 +0300

    Eslint

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,6 +10,9 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -27,7 +30,8 @@ export function register(config) {
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      // serve assets; see
+      // https://github.com/facebook/create-react-app/issues/2374
       return;
     }
 
@@ -35,7 +39,8 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
+        // This is running on localhost.
+        // Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
